3db9ba3ceb5989dde665a3441ef22f04eb272432
eltheuno <eltheuno@gmail.com>
2014-02-25 16:40:43 +0700

    + add AllConstTest.java and PTest.java

---
+++ src/test/java/com/mycompany/testtugaskecilci/AllConstTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.testtugaskecilci;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author BBBE
+ */
+public class AllConstTest extends TestCase {
+    
+    public AllConstTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testAllConst() {
+        AllConst instance = new AllConst();
+        assertEquals(instance.EPSILON, 0.005);
+        assertEquals(instance.PI, 3.1417);
+        assertEquals(instance.RHO, 9.0);
+        assertEquals(instance.N, 8);
+    }
+    
+}
---
+++ src/test/java/com/mycompany/testtugaskecilci/PTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.testtugaskecilci;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author BBBE
+ */
+public class PTest extends TestCase {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    
+    public PTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        System.setOut(null);
+    }
+
+    public void testP(){
+        P instance = new P();
+        assertEquals("ctor-P", outContent.toString().trim());
+    }
+    
+}
